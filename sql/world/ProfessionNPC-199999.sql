cb1c6c59a942b10946ec9a2061a632fe99c38dc4
Stefano Borzì <stefanoborzi32@gmail.com>
2021-06-11 22:21:29 +0200

    Update ProfessionNPC-199999.sql

--- sql/world/ProfessionNPC-199999.sql
+++ sql/world/ProfessionNPC-199999.sql
@@ -2,6 +2,6 @@
 -- Profession NPC
 -- --------------------------------------------------------------------------------------
 
-DELETE FROM `creature_template` WHERE `entry` = 199999;
+DELETE FROM `creature_template` WHERE (`entry` = 199999);
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-(199999, 0, 0, 0, 0, 0, 31833, 0, 0, 0, 'Kaylub', '|cff00ccffProfessions NPC|r', 'Speak', 0, 80, 80, 0, 35, 1, 1, 1.14286, 0.25, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 'CreatureScript_Professions', 0);
+(199999, 0, 0, 0, 0, 0, 31833, 0, 0, 0, 'Kaylub', '|cff00ccffProfessions NPC|r', 'Speak', 0, 80, 80, 0, 35, 1, 1, 1.14286, 0.25, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 'CreatureScript_Professions', 0);
